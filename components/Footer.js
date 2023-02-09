7202ea8cf029cdc49b12c633df2535a57a54fb70
Abhishek <abhishekkumar232105@gmail.com>
2023-02-09 10:40:01 +0530

    navbar Completed

---
+++ components/Footer.js
@@ -0,0 +1,26 @@
+
+import styles from '../styles/footer.module.css'
+
+export default function Footer()
+{
+    return (
+        <div className={styles.footer}>
+
+{/* ****************************************************Content Starts*************************** */}
+
+            <div className={styles.content}>
+ {/* ***********************************Heading ************************************* */}
+           <div className={styles.heading}>
+            <p>
+            HandCrafted by ❤️ © twentytwentythree</p>
+           </div>
+ 
+           
+         </div>
+
+
+        
+         </div>
+    )
+    
+}
